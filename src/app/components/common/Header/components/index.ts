fa9d765e7c25638d932c6b09803f484d050ccd35
NEVI0 <neviocostamagagnin@gmail.com>
2025-05-05 22:22:11 -0300

    feat: adding mobile drawer menu

---
+++ src/app/components/common/Header/components/index.ts
@@ -0,0 +1,1 @@
+export { default as MobileDrawerMenu } from './MobileDrawerMenu';
--- src/app/components/common/Header/index.tsx
+++ src/app/components/common/Header/index.tsx
@@ -1,61 +1,80 @@
+'use client';
+
+import { useState } from 'react';
+
 import Link from 'next/link';
 import { Menu } from 'lucide-react';
 
 import { IconButton, VerticalDivider } from '@app/components/ui';
+import { MobileDrawerMenu } from './components';
 
 export default function Header() {
-  return (
-    <header className="flex items-center justify-center w-full h-[80px] border-b border-gray-300">
-      <div className="flex items-center justify-between h-full w-6xl mx-auto px-6 md:px-8 ">
-        <Link
-          href="/"
-          className="h-full flex items-center justify-center font-medium"
-        >
-          <h1 className="text-2xl font-bold">
-            <span className="text-primary">My</span>Mind Blog
-          </h1>
-        </Link>
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
-        <nav className="hidden md:flex items-center gap-4 h-full">
+  function handleToggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  return (
+    <>
+      <header className="flex items-center justify-center w-full h-[80px] border-b border-gray-300">
+        <div className="flex items-center justify-between h-full w-6xl mx-auto px-6 md:px-8 ">
           <Link
             href="/"
             className="h-full flex items-center justify-center font-medium"
           >
-            Início
+            <h1 className="text-2xl font-bold">
+              <span className="text-primary">My</span>Mind Blog
+            </h1>
           </Link>
 
-          <VerticalDivider />
+          <nav className="hidden md:flex items-center gap-4 h-full">
+            <Link
+              href="/"
+              className="h-full flex items-center justify-center font-medium"
+            >
+              Início
+            </Link>
 
-          <Link
-            href="/posts"
-            className="h-full flex items-center justify-center font-medium"
-          >
-            Publicações
-          </Link>
+            <VerticalDivider />
 
-          <VerticalDivider />
+            <Link
+              href="/posts"
+              className="h-full flex items-center justify-center font-medium"
+            >
+              Publicações
+            </Link>
 
-          <Link
-            href="/#about"
-            className="h-full flex items-center justify-center font-medium"
-          >
-            Sobre o blog
-          </Link>
+            <VerticalDivider />
 
-          <VerticalDivider />
+            <Link
+              href="/#about"
+              className="h-full flex items-center justify-center font-medium"
+            >
+              Sobre o blog
+            </Link>
 
-          <Link
-            href="/account/nevio"
-            className="h-full flex items-center justify-center font-medium text-primary"
-          >
-            Minha conta
-          </Link>
-        </nav>
+            <VerticalDivider />
 
-        <div className="md:hidden">
-          <IconButton icon={<Menu />} />
+            <Link
+              href="/account/nevio"
+              className="h-full flex items-center justify-center font-medium text-primary"
+            >
+              Minha conta
+            </Link>
+          </nav>
+
+          <div className="md:hidden">
+            <IconButton
+              icon={<Menu />}
+              type="button"
+              onClick={handleToggleMenu}
+            />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      <MobileDrawerMenu isOpen={isMenuOpen} onToggle={handleToggleMenu} />
+    </>
   );
 }
