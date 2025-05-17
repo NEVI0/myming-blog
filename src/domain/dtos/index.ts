f0460324b54ed67fd01f03f7d6b24d99daa78ee1
NEVI0 <neviocostamagagnin@gmail.com>
2025-05-17 11:38:02 -0300

    refactor: removing unused files

--- src/domain/dtos/index.ts
+++ src/domain/dtos/index.ts
@@ -1,4 +1,3 @@
 export type { default as CreatePostDTO } from './CreatePostDTO';
-export type { default as FindAllPostsDTO } from './FindAllPostsDTO';
 export type { default as FetchPostByIdDTO } from './FetchPostByIdDTO';
 export type { default as FetchUserByIdDTO } from './FetchUserByIdDTO';
--- src/domain/useCases/index.ts
+++ src/domain/useCases/index.ts
@@ -1,4 +1,3 @@
 export { default as CreatePostUseCase } from './CreatePostUseCase';
 export { default as FetchPostByIdUseCase } from './FetchPostByIdUseCase';
 export { default as FetchUserByIdUseCase } from './FetchUserByIdUseCase';
-export { default as FindAllPostsUseCase } from './FindAllPostsUseCase';
