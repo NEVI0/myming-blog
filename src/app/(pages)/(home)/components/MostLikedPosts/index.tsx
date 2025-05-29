4ba531a294c0919c6ef053d6c2cd7c9475c43a7b
NEVI0 <neviocostamagagnin@gmail.com>
2025-05-28 22:56:12 -0300

    feat: adding loading state

--- src/app/(pages)/(home)/components/MostLikedPosts/index.tsx
+++ src/app/(pages)/(home)/components/MostLikedPosts/index.tsx
@@ -1,8 +1,14 @@
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
 
-import { Section, Post } from '@app/components/common';
+import { Section } from '@app/components/common';
+import { Skeleton } from '@app/components/ui';
 
 export default function MostLikedPosts() {
+  const [isLoading] = useState<boolean>(true);
+
   return (
     <Section
       title="Posts mais curtidos pelos usuário"
@@ -15,11 +21,19 @@ export default function MostLikedPosts() {
         </Link>
       }
     >
-      <div className="grid  gap-4 grid-cols-1 md:grid-cols-2">
-        {new Array(6).fill(0).map((_, index) => (
+      {isLoading ? (
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <Skeleton key={index} className="h-[168px] w-full rounded-3xl" />
+          ))}
+        </div>
+      ) : (
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2">
+          {/* {new Array(6).fill(0).map((_, index) => (
           <Post key={index} />
-        ))}
-      </div>
+        ))} */}
+        </div>
+      )}
     </Section>
   );
 }
