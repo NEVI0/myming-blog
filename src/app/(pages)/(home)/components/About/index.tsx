d733d5431fcde6bf06e62d22d1a3f9745bff5530
NEVI0 <neviocostamagagnin@gmail.com>
2025-05-28 22:56:00 -0300

    feat: adding about section

--- src/app/(pages)/(home)/components/About/index.tsx
+++ src/app/(pages)/(home)/components/About/index.tsx
@@ -1,29 +1,69 @@
+import { ChevronRight } from 'lucide-react';
+
 import { Section } from '@app/components/common';
 
 export default function About() {
   return (
     <Section title="Sobre o blog e seu propósito" id="about">
       <div className="flex flex-col gap-4">
-        <p className="text-gray-600">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-          vulputate tristique massa, in lacinia purus vehicula id. Etiam porta
-          rhoncus massa id suscipit. Ut quam est, sollicitudin vel libero nec,
-          suscipit tristique lectus. Donec ultrices lorem enim, at dignissim
-          massa molestie sit amet.
+        <p>
+          Bem-vindo ao MyMind, um espaço onde suas ideias ganham vida! Aqui,
+          você tem total liberdade para compartilhar seus pensamentos,
+          experiências e conhecimentos com o mundo.
         </p>
 
-        <p className="text-gray-600">
-          Nullam bibendum dignissim lorem sit amet molestie. Sed iaculis ipsum
-          eget libero facilisis, eget lobortis magna luctus. Nulla egestas
-          fermentum accumsan. Nunc placerat aliquam magna a mattis. Nam
-          vestibulum leo at nisi pharetra condimentum. Praesent varius pulvinar
-          ex quis eleifend.
+        <p>
+          Nossa missão é criar uma comunidade vibrante e diversificada, onde
+          cada pessoa pode expressar-se livremente. Seja você um escritor
+          experiente ou alguém que está começando sua jornada na escrita, o
+          MyMind é o lugar perfeito para você.
         </p>
 
-        <p className="text-gray-600">
-          Ut tincidunt elit ipsum, at finibus nibh feugiat ut. Mauris lacinia
-          ligula id aliquam vehicula. Ut sodales velit non leo commodo, sit amet
-          commodo elit lobortis.
+        <p>No MyMind, você pode:</p>
+
+        <ul className="flex flex-col gap-2">
+          <li className="flex items-center gap-4">
+            <div>
+              <ChevronRight className="text-primary size-4" />
+            </div>
+
+            <p>Compartilhar suas histórias e experiências pessoais;</p>
+          </li>
+
+          <li className="flex items-center gap-4">
+            <div>
+              <ChevronRight className="text-primary size-4" />
+            </div>
+
+            <p>Conectar-se com pessoas que compartilham seus interesses;</p>
+          </li>
+
+          <li className="flex items-center gap-4">
+            <div>
+              <ChevronRight className="text-primary size-4" />
+            </div>
+
+            <p>Receber feedback construtivo da comunidade;</p>
+          </li>
+
+          <li className="flex items-center gap-4">
+            <div>
+              <ChevronRight className="text-primary size-4" />
+            </div>
+
+            <p>Explorar diferentes perspectivas e ideias;</p>
+          </li>
+        </ul>
+
+        <p>
+          Acreditamos que cada pessoa tem uma voz única e valiosa para
+          compartilhar. Não importa o assunto - desde viagens e culinária até
+          tecnologia e filosofia - seu conteúdo é bem-vindo aqui.
+        </p>
+
+        <p>
+          Junte-se a nós nesta jornada de descoberta e expressão. Crie sua conta
+          agora e comece a compartilhar suas ideias com o mundo!
         </p>
       </div>
     </Section>
