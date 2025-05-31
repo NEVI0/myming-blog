7879023385d5f2a797302a54b0f95bf5d7a697c8
NEVI0 <neviocostamagagnin@gmail.com>
2025-05-31 13:35:53 -0300

    refactor: improving breadcrumb component

--- src/app/components/ui/Breadcrumb/index.tsx
+++ src/app/components/ui/Breadcrumb/index.tsx
@@ -5,42 +5,83 @@ import { usePathname } from 'next/navigation';
 
 import { VerticalDivider } from '@app/components/ui';
 
+type BreadcrumbItem = {
+  label: string;
+  href: string;
+};
+
+const PATHS_WITH_PARAMS = ['post', 'account'];
+
 const PATH_TRANSLATIONS: Record<string, string> = {
   post: 'Post',
   posts: 'Posts',
   'create-post': 'Novo post',
-  'review-post': 'Revisar post',
   account: 'Perfil',
+  edit: 'Editar',
 };
 
 export default function Breadcrumb() {
   const pathname = usePathname();
   const segments = pathname.split('/').filter(Boolean);
 
-  const items = segments.map((segment, index) => {
-    const href = `/${segments.slice(0, index + 1).join('/')}`;
-    const isLast = index === segments.length - 1;
-    const isPenultimate = index === segments.length - 2;
-    const page = PATH_TRANSLATIONS[segment];
+  const defaultBreadcrumb: Array<BreadcrumbItem | null> = [
+    {
+      label: 'Início',
+      href: '/',
+    },
+  ];
 
-    return (
-      <div className="flex items-center gap-4" key={href}>
-        {isPenultimate ? (
-          <p className="cursor-default">{page}</p>
-        ) : (
-          <Link href={href}>{page}</Link>
-        )}
+  const breadcrumb: Array<BreadcrumbItem | null> = defaultBreadcrumb
+    .concat(
+      segments.map((segment, index) => {
+        const label = PATH_TRANSLATIONS[segment];
+        const hasParams = PATHS_WITH_PARAMS.includes(segment);
 
-        {!isLast && !page && <VerticalDivider />}
-      </div>
-    );
-  });
+        if (hasParams) {
+          const nextSegment = segments[index + 1];
+
+          return {
+            label,
+            href: `/${segment}/${nextSegment}`,
+          };
+        }
+
+        if (!label) return null;
+
+        return {
+          label,
+          href: `/${segment}`,
+        };
+      })
+    )
+    .filter((item) => item !== null);
+
+  function handleGenerateKey() {
+    return Math.random().toString(36).substring(2, 15);
+  }
 
   return (
     <section className="flex items-center gap-4">
-      <Link href="/">Início</Link>
-      {items.length > 0 && <VerticalDivider />}
-      {items}
+      {breadcrumb
+        .filter((item) => item !== null)
+        .map((item, index) => {
+          const isLast = index === breadcrumb.length - 1;
+
+          if (isLast) {
+            return (
+              <Link href={item.href} key={handleGenerateKey()}>
+                {item.label}
+              </Link>
+            );
+          }
+
+          return [
+            <Link href={item.href} key={handleGenerateKey()}>
+              {item.label}
+            </Link>,
+            <VerticalDivider key={handleGenerateKey()} />,
+          ];
+        })}
     </section>
   );
 }
